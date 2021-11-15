04c48a7355ab11a31f4f757e103f8d4f29580991
Postoev-Alexander <postoevalex@gmail.com>
2021-11-15 22:33:27 +0700

    Add simple Upload metod

--- cmd/storage/main.go
+++ cmd/storage/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
-
+	"log"
 	"storage/internal/storage"
+	//"github.com/Postoev-Alexander/storage/internal/storage"
 )
 
 func main() {
 	st := storage.NewStorage()
-	fmt.Println("it work", st)
+
+	file, err := st.Upload("test.txt", []byte("hello"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("it uploaded", file)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module storage
 
 go 1.17
+
+require (
+github.com/google/uuid v1.3.0 
+//github.com/Postoev-Alexander/storage/internal/storage
+)
